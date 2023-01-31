1d51fbca06dd534fd04b395714c8c614624b1d3c
Ahmed Ehab <85330659+ahmedbobh@users.noreply.github.com>
2023-01-31 03:47:23 +0200

    Add files via upload

---
+++ Collect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collect : MonoBehaviour
+{
+    public Text ScoreText;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score=0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="black"){
+            score --;
+            ScoreText.text=score.ToString();
+            other.gameObject.SetActive(false);
+
+        }
+        if(other.gameObject.tag=="white"){
+            score ++;
+             ScoreText.text=score.ToString();
+            other.gameObject.SetActive(false);
+         
+        }
+    }
+}
